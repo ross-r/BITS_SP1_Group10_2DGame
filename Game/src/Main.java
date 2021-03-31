695384cbcfc2ef36a6c26aa9f6db722a5914ac25
Ross <rossrhodesedu@gmail.com>
2021-03-31 11:48:18 +0800

    Initialized NanoVG and draw basic rounded rectangle

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,5 +1,6 @@
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
+import org.lwjgl.nanovg.NVGColor;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
 
@@ -11,12 +12,20 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
-import org.lwjgl.nanovg.*;
+import static org.lwjgl.nanovg.NanoVG.*;
+import static org.lwjgl.nanovg.NanoVGGL3.*;
 
 public class Main {
 
 	// The window handle
 	private long window;
+	
+	// The windows width and height.
+	private int width;
+	private int height;
+	
+	// The NanoVG handle.
+	private long vg;
 
 	public void run() {
 		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
@@ -46,7 +55,9 @@ public class Main {
 		glfwDefaultWindowHints(); // optional, the current window hints are already the default
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
+		glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+		glfwWindowHint(GLFW_SAMPLES, 16); // 16x MSAA
+		
 		// Create the window
 		window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
 		if ( window == NULL )
@@ -69,19 +80,24 @@ public class Main {
 			// Get the resolution of the primary monitor
 			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
+			// Store off the width and height for later usage.
+			width = pWidth.get();
+			height = pHeight.get();
+			
 			// Center the window
 			glfwSetWindowPos(
 				window,
-				(vidmode.width() - pWidth.get(0)) / 2,
-				(vidmode.height() - pHeight.get(0)) / 2
+				(vidmode.width() - width) / 2,
+				(vidmode.height() - height) / 2
 			);
 		} // the stack frame is popped automatically
 
 		// Make the OpenGL context current
 		glfwMakeContextCurrent(window);
+		
 		// Enable v-sync
 		glfwSwapInterval(1);
-
+		
 		// Make the window visible
 		glfwShowWindow(window);
 	}
@@ -94,20 +110,41 @@ public class Main {
 		// bindings available for use.
 		GL.createCapabilities();
 
-		// Set the clear color
-		glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-
+		// Setup NanoVG.
+		vg = nvgCreate(NVG_ANTIALIAS);
+        if (vg == NULL) {
+            throw new RuntimeException("Could not init nanovg.");
+        }
+		
 		// Run the rendering loop until the user has attempted to close
 		// the window or has pressed the ESCAPE key.
 		while ( !glfwWindowShouldClose(window) ) {
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
+			glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+			
+			final NVGColor color = NVGColor.create();
+			color.r(255.F / 255.F);
+			color.g(255.F / 255.F);
+			color.b(255.F / 255.F);
+			color.a(127.F / 255.F);
+			
+			nvgBeginFrame(vg, width, height, 96.F);
+			
+			nvgBeginPath(vg);
+			nvgRoundedRect(vg, 20, 20, width - 40, height - 40, 30.F);
+			nvgFillColor(vg, color);
+			nvgFill(vg);
+			
+			nvgEndFrame(vg);
+			
 			glfwSwapBuffers(window); // swap the color buffers
 
 			// Poll for window events. The key callback above will only be
 			// invoked during this call.
 			glfwPollEvents();
 		}
+		
+		nvgDelete(vg);
 	}
 
 	public static void main(String[] args) {
