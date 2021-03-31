1f17a2d74e3f3e6eeb4c520187e5b6b97e6ad511
Ross <rossrhodesedu@gmail.com>
2021-03-31 14:15:39 +0800

    Separated creation routine into Window class

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,154 +1,68 @@
 import org.lwjgl.*;
-import org.lwjgl.glfw.*;
 import org.lwjgl.nanovg.NVGColor;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
 
 import static org.lwjgl.nanovg.NanoVG.*;
 import static org.lwjgl.nanovg.NanoVGGL3.*;
 
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 public class Main {
 
-	// The window handle
-	private long window;
+	private final int WINDOW_WIDTH = 1280;
+	private final int WINDOW_HEIGHT = 720;
 	
-	// The windows width and height.
-	private int width;
-	private int height;
+	private Window window;
 	
-	// The NanoVG handle.
+	// Temporary NanoVG stuff.
 	private long vg;
+	private NVGColor color;
 
-	public void run() {
+	public void start() {
 		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
-		init();
-		loop();
-
-		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
-	}
-
-	private void init() {
-		// Setup an error callback. The default implementation
-		// will print the error message in System.err.
-		GLFWErrorCallback.createPrint(System.err).set();
-
-		// Initialize GLFW. Most GLFW functions will not work before doing this.
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Configure GLFW
-		glfwDefaultWindowHints(); // optional, the current window hints are already the default
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-		glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
-		glfwWindowHint(GLFW_SAMPLES, 16); // 16x MSAA
+		// Create our window.
+		window = new Window("Trash Unit Response Droid (T.U.R.D)", WINDOW_WIDTH, WINDOW_HEIGHT);
 		
-		// Create the window
-		window = glfwCreateWindow(1280, 720, "Hello World!", NULL, NULL);
-		if ( window == NULL )
-			throw new RuntimeException("Failed to create the GLFW window");
-
-		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-		});
-
-		// Get the thread stack and push a new frame
-		try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1); // int*
-			IntBuffer pHeight = stack.mallocInt(1); // int*
-
-			// Get the window size passed to glfwCreateWindow
-			glfwGetWindowSize(window, pWidth, pHeight);
-
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-			// Store off the width and height for later usage.
-			width = pWidth.get();
-			height = pHeight.get();
-			
-			// Center the window
-			glfwSetWindowPos(
-				window,
-				(vidmode.width() - width) / 2,
-				(vidmode.height() - height) / 2
-			);
-		} // the stack frame is popped automatically
-
-		// Make the OpenGL context current
-		glfwMakeContextCurrent(window);
-		
-		// Enable v-sync
-		glfwSwapInterval(1);
-		
-		// Make the window visible
-		glfwShowWindow(window);
-	}
-
-	private void loop() {
-		// This line is critical for LWJGL's interoperation with GLFW's
-		// OpenGL context, or any context that is managed externally.
-		// LWJGL detects the context that is current in the current thread,
-		// creates the GLCapabilities instance and makes the OpenGL
-		// bindings available for use.
-		GL.createCapabilities();
-
-		// Setup NanoVG.
+		// Now that we have the window created we can initialize NanoVG.
+		// TODO: This would be in its own class, something like Graphics.setup();
 		vg = nvgCreate(NVG_ANTIALIAS);
-        if (vg == NULL) {
-            throw new RuntimeException("Could not init nanovg.");
+		if (vg == NULL) {
+			throw new RuntimeException("Could not create nanovg.");
         }
 		
-		// Run the rendering loop until the user has attempted to close
-		// the window or has pressed the ESCAPE key.
-		while ( !glfwWindowShouldClose(window) ) {
-			glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		// Initialize color instance.
+		// This allocates 4 bytes in memory.
+		color = NVGColor.create();
+		
+		window.render(() -> {
 			
-			final NVGColor color = NVGColor.create();
+			// Begin the frame, setup NanoVG for rendering and set opengl states and buffers.
+			// TODO: @Ross, set device pixel ratio properly.
+			nvgBeginFrame(vg, window.getScaledWidth(), window.getScaledHeight(), window.getPixelRatio());
+			
+			// Render some stuff.
 			color.r(255.F / 255.F);
 			color.g(0.F / 255.F);
 			color.b(0.F / 255.F);
 			color.a(127.F / 255.F);
 			
-			nvgBeginFrame(vg, width, height, 96.F);
-			
 			nvgBeginPath(vg);
-			nvgRoundedRect(vg, 20, 20, width - 40, height - 40, 30.F);
+			nvgRoundedRect(vg, 0, 0, 80, 80, 9.F);
 			nvgFillColor(vg, color);
 			nvgFill(vg);
 			
+			// End the frame, this will set some opengl states and clear buffers.
 			nvgEndFrame(vg);
 			
-			glfwSwapBuffers(window); // swap the color buffers
-
-			// Poll for window events. The key callback above will only be
-			// invoked during this call.
-			glfwPollEvents();
-		}
+		}, 0.3F, 0.3F, 0.32F, 1.F);
 		
+		// Terminate the window and cleanup NanoVG context.
+		window.terminate();
 		nvgDelete(vg);
 	}
 
 	public static void main(String[] args) {
-		new Main().run();
+		new Main().start();
 	}
 
 }
---
+++ Game/src/Window.java
@@ -0,0 +1,231 @@
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+public class Window {
+
+	// The handle to our window.
+	private long hHandle;
+	
+	// The width and height supplied for window creation.
+	private int iWidth;
+	private int iHeight;
+	
+	// The internal width and height of the window, commonly referred as frame width/height.
+	// The width and height here are used for frame buffers, that is, internal viewports and such for rendering.
+	private int iFrameBufferWidth;
+	private int iFrameBufferHeight;
+	
+	//
+	private float flContentScaleX;
+	private float flContentScaleY;
+	
+	// The window title.
+	private String sTitle;
+	
+	// Default window properties that can be overloaded by a different constructor.
+	// TODO: @Ross, add other constructor to initialize values.
+	private int iResizable;
+	private int iVisible;
+	private int iMSAAQuality;
+	
+	public Window(String title, int w, int h) {
+		this.sTitle = title;
+		this.iWidth = w;
+		this.iHeight = h;
+
+		//
+		// Default properties.
+		//
+		
+		// Allow the window to be resized.
+		iResizable = GLFW_TRUE;
+		
+		// Is this window visible?
+		iVisible = GLFW_TRUE;
+		
+		// Enable Multisample anti-aliasing.
+		// This value must be 2^n
+		iMSAAQuality = 16;
+		
+		this.initAndCreateWindow();
+	}
+	
+	private void updateWindowSizes() {
+		try(MemoryStack stack = stackPush()) {
+			
+			// Get the frame buffer sizes.
+			IntBuffer fw = stack.mallocInt(1);
+			IntBuffer fh = stack.mallocInt(1);
+
+			glfwGetFramebufferSize(hHandle, fw, fh);
+			this.iFrameBufferWidth = fw.get();
+			this.iFrameBufferHeight = fh.get();
+			
+			// Get the content scales.
+			FloatBuffer sx = stack.mallocFloat(1);
+			FloatBuffer sy = stack.mallocFloat(1);
+			
+			glfwGetWindowContentScale(hHandle, sx, sy);
+			this.flContentScaleX = sx.get();
+			this.flContentScaleY = sy.get();
+		}
+	}
+	
+	private void initAndCreateWindow() {
+		// Setup an error callback. The default implementation
+		// will print the error message in System.err.
+		GLFWErrorCallback.createPrint(System.err).set();
+		
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if (!glfwInit()) {
+			throw new IllegalStateException("Unable to initialize GLFW");
+		}
+
+		//
+		// Configure GLFW
+		//
+		
+		// optional, the current window hints are already the default
+		glfwDefaultWindowHints();
+		
+		glfwWindowHint(GLFW_VISIBLE, iVisible);
+		glfwWindowHint(GLFW_RESIZABLE, iResizable);
+		
+		/*
+		 GLFW_SCALE_TO_MONITOR specified whether the window content area should be resized based on the monitor content scale of any monitor it is placed on. 
+		 This includes the initial placement when the window is created. Possible values are GLFW_TRUE and GLFW_FALSE.
+		 
+		 This is for larger resolution monitors like 1440p, 4K and retina displays.
+		 */
+		glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+		
+		// TODO: @Ross, temporarily disabled as Leo has window creation failures on macOS.
+		// 				this may be the cause of those issues.
+		//glfwWindowHint(GLFW_SAMPLES, iMSAAQuality);
+		
+		//
+		// Window creation.
+		//
+		
+		// Create the window.
+		this.hHandle = glfwCreateWindow(iWidth, iHeight, sTitle, NULL, NULL);
+
+		// Update the window width and height and internal buffer sizes.
+		updateWindowSizes();
+		
+		// Get the video mode for the primary display.
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		
+		// Center the window on the primary monitor.
+		glfwSetWindowPos(hHandle, (vidmode.width() - iWidth) / 2, (vidmode.height() - iHeight) / 2);	
+
+		// Make the OpenGL context current.
+		glfwMakeContextCurrent(hHandle);
+		
+		// Enable v-sync.
+		// TODO: @Ross, make this optional.
+		glfwSwapInterval(1);
+		
+		// Make the window visible.
+		glfwShowWindow(hHandle);
+		
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		GL.createCapabilities();
+		
+		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
+		glfwSetKeyCallback(hHandle, (window, key, scancode, action, mods) -> {
+			
+			// We'll use this to notify the input class of changes.
+			inputCallback(window, key, scancode, action, mods);
+		});
+		
+		// Setup a size callback.
+		glfwSetWindowSizeCallback(hHandle, (window, width, height) -> {
+			this.iWidth = width;
+			this.iHeight = height;
+			
+			updateWindowSizes();
+		});
+	}
+	
+	public void render(Runnable callback, float r, float g, float b, float a) {
+		
+		// TODO: @Ross, render statistics (fps, frame time, etc..)
+		
+		while(!glfwWindowShouldClose(hHandle)) {
+		
+			// Set the clear color.
+			glClearColor(r, g, b, a);
+			
+			// Clear a couple buffers that NanoVG and other libraries may touch.
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		
+			// Make sure to update the viewport for window resizing.
+			glViewport(0, 0, this.iFrameBufferWidth, this.iFrameBufferHeight);
+			
+			// If we have a render callback execute it now.
+			if(callback != null) {
+				callback.run();
+			}
+			
+			glfwSwapBuffers(hHandle);
+			
+			// Poll for window events. The key callback above will only be invoked during this call.
+			glfwPollEvents();
+		}
+	}
+	
+	public void terminate() {
+		// Free the window callbacks and destroy the window
+		glfwFreeCallbacks(hHandle);
+		glfwDestroyWindow(hHandle);
+
+		// Terminate GLFW and free the error callback
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+	
+	private void inputCallback(long window, int key, int scancode, int action, int mods) {
+		// TODO: Implement input class.		
+	}
+
+	//
+	// Getters/Setters
+	//
+	
+	public int getWidth() {
+		return iWidth;
+	}
+	
+	public int getHeight() {
+		return iHeight;
+	}
+	
+	public int getScaledWidth() {
+		return (int)(this.iFrameBufferWidth / this.flContentScaleX);
+	}
+	
+	public int getScaledHeight() {
+		return (int)(this.iFrameBufferHeight / this.flContentScaleY);
+	}
+	
+	public int getPixelRatio() {
+		int a = getScaledWidth();
+		int b = getScaledHeight();
+		return a > b ? a : b;
+	}
+}
