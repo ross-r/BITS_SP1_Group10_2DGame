c714ac4b8911ee470f6be8f1ddd1b57ae60019ef
Hamilton <70889510+hamiltonhunter@users.noreply.github.com>
2021-04-09 16:16:15 +0800

    Changed platform locations and dimentions

--- Game/src/turd/game/Main.java
+++ Game/src/turd/game/Main.java
@@ -14,50 +14,52 @@ public class Main {
 
 	private final int WINDOW_WIDTH = 1280;
 	private final int WINDOW_HEIGHT = 720;
-	
+
 	private Window window;
 	private Graphics graphics;
-	
+
 	public void start() {
 		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
 		// Create our window.
 		window = new Window("Trash Unit Response Droid (T.U.R.D)", WINDOW_WIDTH, WINDOW_HEIGHT);
-		
+
 		// Create graphics.
 		graphics = new Graphics(window, NVG_ANTIALIAS);
-		
+
 		// Create our player.
 		ObjectList.getInstance().createPlayer();
-		
+
 		// Setup an obstacle that the player cannot pass through.
-		StaticObject obstacle = ObjectList.getInstance().createStaticObject();
-		obstacle.setPos(0, 250);
-		obstacle.setBounds(200, 8);
-		
+
+		StaticObject obstacle0 = ObjectList.getInstance().createStaticObject();
+		obstacle0.setPos(0, 660);
+		obstacle0.setBounds(1280, 60);
+
 		StaticObject obstacle1 = ObjectList.getInstance().createStaticObject();
-		obstacle1.setPos(300, 250);
-		obstacle1.setBounds(200, 8);
-		
+		obstacle1.setPos(0, 260);
+		obstacle1.setBounds(60, 400);
+
 		StaticObject obstacle2 = ObjectList.getInstance().createStaticObject();
-		obstacle2.setPos(300, 0);
-		obstacle2.setBounds(8, 250);
-		
+		obstacle2.setPos(700, 460);
+		obstacle2.setBounds(300, 200);
+
 		window.render(() -> {
-			
+
 			graphics.beginFrame();
 
 			// Draw FPS/Render time stats.
 			graphics.setColor(255.f, 255.f, 255.f, 255.f);
-			graphics.drawString(String.format("Trash Unit Response Droid (T.U.R.D)\nFPS: %.0f (%.3f m/s)", window.getFps(), window.getFrameTime() * 1000.0), 2, 2, 1.f);
-			
+			graphics.drawString(String.format("Trash Unit Response Droid (T.U.R.D)\nFPS: %.0f (%.3f m/s)",
+					window.getFps(), window.getFrameTime() * 1000.0), 2, 2, 1.f);
+
 			// Render all objects.
 			ObjectList.getInstance().render(window, graphics);
-			
+
 			graphics.endFrame();
-			
+
 		}, 0.3F, 0.3F, 0.32F, 1.F);
-		
+
 		// Terminate the window and cleanup NanoVG context.
 		window.terminate();
 		graphics.terminate();
