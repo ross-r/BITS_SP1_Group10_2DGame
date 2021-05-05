877b343afdb19381e7c3208f876dff8052f1f9c2
MichaelMcQuarrie <s3884359@student.rmit.edu.au>
2021-05-05 11:07:00 +1000

    Update Projectile.java

--- Game/src/turd/game/entities/Projectile.java
+++ Game/src/turd/game/entities/Projectile.java
@@ -58,19 +58,21 @@ public class Projectile extends GameObject{
 		flxDirection = (float)(Math.sin(Math.toRadians(getRotation())) * this.flProjectileSpeed);
 		flyDirection = (float)(Math.cos(Math.toRadians(getRotation())) * -this.flProjectileSpeed);
 		
+		//Moving box
 		this.physics.move(this.aabb.p0.x + flxDirection, this.aabb.p0.y + flyDirection);
 		
-		
+		//If it collides delete
 		if(flOldX == this.aabb.p0.x && flOldY == this.aabb.p0.y) {
 			this.flProjectileSpeed = 0;
 			System.out.println("Collsion Deleted");
 		}
 		
+		//If it times out or goes off screen delete
 		if(this.iTimeonScreen == 2000) {
 			this.flProjectileSpeed = 0;
 			System.out.println("Projectile Time Deleted");
 		}
-		
+		//add to time counter
 		this.iTimeonScreen = this.iTimeonScreen + (int)this.flProjectileSpeed;
 		
 		
@@ -84,20 +86,19 @@ public class Projectile extends GameObject{
 		float flXExtended = 0;
 		float flYExtended = 0;
 		
-		
+		//Making the box
 		this.aabb.init(iStartX - (PLAYER_BOUNDS / 2), iStartY - (PLAYER_BOUNDS / 2), PLAYER_BOUNDS, PLAYER_BOUNDS);
 		
-		
+		//Setting origin
 		iStartX = this.iOriginX;
 		iStartY = this.iOriginY;
 		
-		System.out.println("X1: " + iStartX + " Y1: " + iStartY + " X2: " + iDesX +  " Y2: " + iDesY);
+		//System.out.println("X1: " + iStartX + " Y1: " + iStartY + " X2: " + iDesX +  " Y2: " + iDesY);
 		this.iDestinationX = iDesX;
 		this.iDestinationY = iDesY;
 		
-		System.out.println(getRotation());
+		//System.out.println(getRotation());
 		//Trig to extend the line out to keep going
-		float flRotation = getRotation();
 		flLength = (float)Math.sqrt(Math.pow(this.iOriginX - this.iDestinationX, 2) + Math.pow(this.iOriginY - this.iDestinationY, 2));
 		
 		flLength = flLength * 10.0f;
@@ -109,11 +110,12 @@ public class Projectile extends GameObject{
 		//flXExtended = (float)(this.iDestinationX + 500 * Math.sin(getRotation()));
 		//flYExtended = (float)(this.iDestinationY + 500 * Math.cos(getRotation()));
 		
+		//Setting extended line
 		this.iDestinationX = (int)flXExtended;
 		this.iDestinationY = (int)flYExtended;
 		
-		System.out.println("X1: " + this.iOriginX + " Y1: " + this.iOriginY + " X2: " + this.iDestinationX  +  " Y2: " + this.iDestinationY);
-		System.out.println(getRotation());
+		//System.out.println("X1: " + this.iOriginX + " Y1: " + this.iOriginY + " X2: " + this.iDestinationX  +  " Y2: " + this.iDestinationY);
+		//System.out.println(getRotation());
 	}
 	
 	public float getProjectileSpeed() {
